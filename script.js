5a215f6e1776782c65aed63b06105e5f43694478
tani27 <70065103+tani27@users.noreply.github.com>
2021-08-10 19:22:24 +0530

    my second commit

--- script.js
+++ script.js
@@ -1,19 +1,47 @@
 $(document).ready(function(){
    $("#search").on("keyup",function(e){
     var username = e.target.value;
-
-    $.ajax({
+    
+    if(username == "")
+    {
+        $("#user").html("");
+    }
+    else{$.ajax({
+        type: 'GET',
         url: "https://api.github.com/users/"+username,
+        dataType: 'json',
+        contentType: 'json',
         data:{
-            client_id: 'c1de6864a4c607f077ad',
-            client_secret: "2d7187472400852cd85a1fcc6f22b0cd21d49897"
+            client_id: '68d7fe111b48ca850e15',
+            client_secret: "a48bd10009fdff92b060c3c6c14f4b1430f89686"
         }
     }).done(function(user){
+            console.log(user);
             var name = user.name;
-            $("#user").html(`<div>${name}</div>`)
-    });
-        
-        
+            $("#user").html(`
+            <div>
+                <table>
+                <tr>
+                <td><img style = "height : 150px; width : 150px;" src = ${user.avatar_url} alt = "didn't work"></td>
+                <td>
+                <td style = "padding-left : 10px;font-size : 20px">repository : ${user.public_repos}</td>
+                <td style = "padding-left : 10px; font-size : 20px">followers : ${user.followers}</td>
+                </tr>
+                <tr>
+                <td style = "font-size : 25px"><p><strong>${user.name}</strong></p></td>
+                </tr>
+                </table>
+            </div>
+            <a target = "_blank" href =  "${user.html_url}"<p style = "margin-left: 200px; font-size: 30px">click here to view whole profile </p></a>
+            `)
+    });   
+    }
+    
+    
     
    });
+
+  
+
+
 });
